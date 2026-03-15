7aeb2e994fb0e07758105ba61eb23f0b8393c012
Baker <9544210+dragonmost@users.noreply.github.com>
2026-03-14 22:09:31 -0400

    bump version

--- userscript/amazon-camel-link.user.js
+++ userscript/amazon-camel-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Camelzone
 // @namespace    https://github.com/Dragonmost/Camelzone
-// @version      0.3.0
+// @version      0.4.0
 // @description  Adds a CamelCamelCamel button on Amazon product pages.
 // @author       Dragonmost
 // @match        https://www.amazon.ca/*
